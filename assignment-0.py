f16e19194681833f487224c5e5cdaf2c94e00bd3
annedorene <99144347+annedorene@users.noreply.github.com>
2022-05-30 17:38:29 +0800

    Add files via upload

---
+++ assignment-0.py
@@ -0,0 +1,49 @@
+'''Assignment 0
+
+This assignment is a giveaway assignment.
+By completing this assignment, you will familiarize yourself with this
+    course's submission mechanics.
+'''
+
+#def three_number_average(x, y, z):
+'''Item 1.
+    Three Number Average. 3 points.
+
+    Returns the average of three numbers.
+
+    Parameters
+    ----------
+    x: int
+        the first number
+    y: int
+        the second number
+    z: int
+        the third number
+
+    Returns
+    -------
+    float
+        the average of x, y, and z
+    '''
+    # Write your code below this
+
+def three_number_average(x, y, z):
+    x = 2
+    y = 4
+    z = 6
+    average = (x+y+z)/3
+    return average
+
+x = 2
+y = 4
+z = 6
+average = three_number_average(x, y, z) 
+
+
+
+print(average)
+
+
+
+    
+        
